df8f3ba8464802cae398818a679e2062bce1cfc5
y1cao <y1cao@lincl17.cs.csbsju.edu>
2016-03-29 10:19:44 -0500

    Homework

---
+++ tests/edu/csbsju/ntc/NewTelephoneCompanyTest_WBT.java
@@ -0,0 +1,39 @@
+package edu.csbsju.ntc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NewTelephoneCompanyTest_WBT {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testComputeCharge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDuration() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDuration() {
+		fail("Not yet implemented");
+	}
+
+}
